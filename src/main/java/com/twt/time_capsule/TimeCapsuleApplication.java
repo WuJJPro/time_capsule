011d22be0410666fc6529cded88de362252be203
Wjj <280933089@qq.com>
2022-10-03 13:21:06 +0800

    首次提交

---
+++ src/main/java/com/twt/time_capsule/TimeCapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.twt.time_capsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeCapsuleApplication.class, args);
+    }
+
+}
